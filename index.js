2ee70b0200ee1e18fb381cca60b1ca81b6481065
unknown <kris25184@gmail.com>
2019-03-09 11:51:50 +0800

    myApp

--- index.js
+++ index.js
@@ -19,8 +19,11 @@ var bot = linebot({
 // 機器人接受訊息的處理
 //----------------------------------------
 bot.on('message', function(event) {
-    console.log("**************");
-    event.reply('Hello, 你好你好');  
+    event.source.profile().then(
+        function (profile) {		
+            return event.reply('你好, ' + profile.displayName + '. 你的編號是:' + profile.userId + ', 你的回應是:' +  event.message.text);
+        }
+    );
 });
 
 
