0eeb08171de274ded1f07717417f0f76c0bdcf18
wkris4233 <10736029@ntub.edu.tw>
2019-12-13 13:45:12 +0800

    edit

--- utility/findAddre.js
+++ utility/findAddre.js
@@ -18,7 +18,7 @@ var fetchAddre = async function(addreKey){
     //substring('addreKey' from 0 for (position('路' in addreKey)))
     console.log(b);
     //讀取資料庫
-    await query('SELECT * FROM "hosp" WHERE hosp."hospAddr" LIKE $1 LIMIT 4',['%'+b+'%'])
+    await query('SELECT * FROM "hosp" WHERE hosp."hospAddr" LIKE $1 LIMIT 4',[b+'%'])
         .then((data) => {
             
             if(data.rows.length > 0){
